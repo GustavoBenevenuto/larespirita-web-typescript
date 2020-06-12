3c9401222c7f73d411d8099c676dcb647b092b99
Gustavo Benevenuto <gustavo.benesilva@gmail.com>
2020-06-12 14:24:51 -0300

    Conclusão da página principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import ImgPray from '../src/assets/pray.png';
 import './App.css';
 
 function App() {
+  const [loadding, setLoadding] = useState(false);
+
+  const handleClick = () => {
+    setLoadding(!loadding);
+  }
+
+  useEffect(() => {
+    console.log(loadding);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header className="bg-info py-5 mb-5">
+        <div className="container h-100">
+          <div className="row h-100 align-items-center">
+            <div className="col-lg-12">
+              <h1 className="display-4 text-white mt-5 mb-2"> 
+              <img src={ImgPray} width={70} alt="" />Lar Espiríta</h1>
+              <p className="lead mb-5 text-white-50">Ajudando pessoas a escontrarem lugares onde poderão praticar o auto-conhecimento e a caridade para como o próximo.</p>
+            </div>
+          </div>
+        </div>
       </header>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 mb-5">
+            <h2>Casa Espírita</h2>
+            <p>Uma casa espírita é uma entidade filantrópica que desenvolve suas atividades com base na Doutrina Espírita,codificada nas obras básicas de Allan Kardec.</p>
+            <a className="btn btn-info btn-lg" href="#">Encontrar &raquo;</a>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
